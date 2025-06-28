1563a88ee3f5b9f9422cc86370331669b6d546ac
Leandro_Jacome_WPOSS <leandrojacome@wposs.com>
2025-06-28 09:39:32 -0500

    deshabilitar cors para pruebas

--- src/main/java/com/united/coders/cmsuser/configuration/security/MainSecurity.java
+++ src/main/java/com/united/coders/cmsuser/configuration/security/MainSecurity.java
@@ -45,7 +45,7 @@ public class MainSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtEntryPoint))
                 .authorizeHttpRequests(auth -> auth
